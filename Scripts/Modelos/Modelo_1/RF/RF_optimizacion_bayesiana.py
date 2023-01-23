86dfa1c0f758cba6203ffbb376246b9198f819fe
matiaspoullain <58349277+matiaspoullain@users.noreply.github.com>
2023-01-23 17:13:26 +0100

    modificacion de optimizacion

--- Scripts/Modelos/Modelo_1/RF/RF_optimizacion_bayesiana.py
+++ Scripts/Modelos/Modelo_1/RF/RF_optimizacion_bayesiana.py
@@ -19,7 +19,7 @@ from bayes_opt import BayesianOptimization
 from bayes_opt.logger import JSONLogger
 from bayes_opt.event import Events
 #import scipy #version 1.7.3 despues de la 1.8 rompe la bayesian optimization
-from Scripts.Modelos.Modelo_1.RF.funciones import estimador, black_box_function
+#from Scripts.Modelos.Modelo_1.RF.funciones import estimador, black_box_function
 
 seed(10)
 datos = pd.read_csv('Datos/Insumos_python/insumo_modelo_1.csv')
@@ -56,6 +56,37 @@ os.makedirs(carpeta_logs, exist_ok=True)
 
 archivo_log = os.path.join(carpeta_logs, 'Optimizacion_bayesiana_logs.json')
 logger = JSONLogger(path=archivo_log)
+
+##### Copiado de funciones:
+
+def estimador(parametros):
+        parametros_it = parametros.copy()
+        parametros_it['n_estimators'] = int(parametros_it['n_estimators'])
+        dict_criterion = {
+            0: 'squared_error',
+            1: 'friedman_mse',
+            2: 'poisson'    
+        }
+        parametros_it['criterion'] = dict_criterion[int(parametros_it['criterion'])]
+        return RandomForestRegressor(**parametros_it, n_jobs = -1)
+
+#Black box function para la optimizacion bayesiana del modelo RF
+#Usa variables definidas por fuera, no le pude encontrar otra vuelta
+def black_box_function(criterion, n_estimators, min_samples_split, max_features):
+    parametros_opt = locals()
+    semillas = [randint(0, 1000000) for _ in range(5)]
+    scores = np.array([])
+    for semilla in semillas:
+        parametros_opt['random_state'] = semilla
+        clf = estimador(parametros = parametros_opt)
+        score_it = cross_val_score(estimator = clf, X = x_train, y = y_train, cv=cv, scoring='neg_root_mean_squared_error')
+        scores = np.append(scores, score_it.mean())
+    return scores.mean()
+
+####
+
+
+
 
 optimizer = BayesianOptimization(f = black_box_function,
                                 pbounds = pbounds, 
