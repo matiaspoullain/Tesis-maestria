ab906a73bff547575a37d76ae9908d9cf19ee927
Matias Poullain <matias.poullain@gmail.com>
2023-02-16 02:02:27 +0100

    xgb funcionando

--- Scripts/Modelos/Modelo_1/XGBoost/XGB_optimizacion_bayesiana.py
+++ Scripts/Modelos/Modelo_1/XGBoost/XGB_optimizacion_bayesiana.py
@@ -16,6 +16,8 @@ import datetime as dt
 from bayes_opt import BayesianOptimization
 from bayes_opt.logger import JSONLogger
 from bayes_opt.event import Events
+from bayes_opt.util import load_logs
+import shutil
 #import scipy #version 1.7.3 despues de la 1.8 rompe la bayesian optimization
 from Scripts.Modelos.Modelo_1.XGBoost.funciones import estimador, black_box_function
 
@@ -52,15 +54,25 @@ carpeta_logs = os.path.join('Modelos', 'logs', 'XGB')
 os.makedirs(carpeta_logs, exist_ok=True)
 
 archivo_log = os.path.join(carpeta_logs, 'Optimizacion_bayesiana_logs.json')
-logger = JSONLogger(path=archivo_log)
 
-
-optimizer = BayesianOptimization(f = black_box_function(x_train, y_train, cv),
+optimizer = BayesianOptimization(f = black_box_function(x_train, y_train, cv, archivo_log),
                                 pbounds = pbounds, 
                                 verbose = 2,
-                                random_state = 10)
+                                random_state = 10,
+                                allow_duplicate_points=True)
 
+if os.path.isfile(archivo_log):
+    print('Archivo log pre-existente, se usa su informacion')
+    load_logs(optimizer, logs=[archivo_log])
+    shutil.copyfile(archivo_log, archivo_log.replace('.json', '_guardado.json'))
+else:
+    print('Archivo log no encontrado, se empieza de 0')
+    
+logger = JSONLogger(path=archivo_log)
 optimizer.subscribe(Events.OPTIMIZATION_STEP, logger)
+
+print(f'Comienza optimizacion bayesiana de {init_points} iteraciones de exploracion y {n_iter} de mejoramiento')
+print(f'Los logs son guardados en {archivo_log}')
 
 optimizer.maximize(init_points = init_points, n_iter = n_iter)
 
